fa16d7caf672c5851f4d6fffcdd05b7078180ec1
Etienne Dusseault <etienne.dusseault@gmail.com>
2018-12-07 23:21:01 +0800

    added oracle and gateway API

---
+++ oracle/oracle.js
@@ -0,0 +1,69 @@
+const Web3 = require('web3')
+const fs = require('fs')
+const { Client, LocalAddress, CryptoUtils, LoomProvider } = require('loom-js')
+const LoomTruffleProvider = require('loom-truffle-provider')
+
+const privateKey = CryptoUtils.generatePrivateKey()
+const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
+
+const chainId = 'default'
+const writeUrl = 'http://127.0.0.1:46658/rpc'
+const readUrl = 'http://127.0.0.1:46658/query'
+
+// Create the client
+const client = new Client(
+    'default',
+    'ws://127.0.0.1:46658/websocket',
+    'ws://127.0.0.1:46658/queryws',
+)
+
+// The address for the caller of the function
+const from = LocalAddress.fromPublicKey(publicKey).toString()
+
+const contractJSON = JSON.parse(fs.readFileSync('./build/contracts/Dividends.json', 'utf-8'))
+const ABI = contractJSON.abi
+const contractAddress = contractJSON.networks.default.address
+
+// Instantiate web3 client using LoomProvider
+const web3 = new Web3(new LoomProvider(client, privateKey))
+
+const dividendsContract = new web3.eth.Contract(ABI, contractAddress, { from })
+
+const loomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
+const loomProvider = loomTruffleProvider.getProviderEngine()
+
+const localAddr = (LocalAddress.fromPublicKey(publicKey).toString())
+
+dividendsContract.methods.calculateDividends(localAddr, 4).send()
+
+
+const testWeb3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+dividendsContract.events.dividendsCalculated({}, (err, event) => {
+    if (err) {
+        return console.error(err)
+    }
+
+    console.log(event.returnValues[1])
+})
+
+
+
+
+
+const oracle = {
+    web3Inst: null,
+    contractInst: {},
+    coinbase: null,
+    initializeEvent: null,
+    contractAddress: null,
+
+    listenForEvents: function () {
+        oracle.contractInst.lotteryContract.lottoInitialized({}, {
+            fromBlock: '0',
+            toBlock: 'latest'
+        }).watch(function (error, event) {
+            console.log(event)
+        })
+    }
+}
