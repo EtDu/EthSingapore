794999623cec7ff97954401c24f15277fdfe04a2
Etienne Dusseault <etienne.dusseault@gmail.com>
2018-12-08 10:51:40 +0800

    modified migrations

---
+++ migrations/3_kovan_contracts.js
@@ -0,0 +1,16 @@
+const securityCoin = artifacts.require('./SecurityCoin.sol')
+
+module.exports = function (deployer, network, accounts) {
+    if (network !== 'kovan') {
+        return
+    }
+
+    deployer.then(async () => {
+        await deployer.deploy(securityCoin)
+        const securityCoinInstance = await securityCoin.deployed()
+
+        console.log('\n*************************************************************************\n')
+        console.log(`securityCoin Kovan Contract Address: ${securityCoinInstance.address}`)
+        console.log('\n*************************************************************************\n')
+    })
+}
