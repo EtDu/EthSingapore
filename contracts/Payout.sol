5881a4aa1567ff8ab0298425619c5ecf989cc247
Clement <clement2296@gmail.com>
2018-12-09 01:57:17 +0800

    Modifications to Payout.sol and SecurityCoin.sol

--- contracts/Payout.sol
+++ contracts/Payout.sol
@@ -6,56 +6,42 @@ import "./SafeMath.sol";
 contract Payout is Ownable {
     using SafeMath for uint256;
 
-    event rateUpdate(bytes32 rate, uint newValue);
-    event securityBalanceUpdate(address owner, uint amount);
-    event dividendsCalculated(address owner, uint amount);
-    event payoutsWithdrawn(address owner, uint amount);
+    event dividendsCalculated(address owner, uint256 amount, uint256 total);
+    event payoutsWithdrawn(address owner, uint256 amount);
 
     mapping(address => mapping(address => uint256)) securityBalance;
     mapping(address => mapping(address => uint256)) lastUpdated;
-    mapping(address => uint256) dividends;
+    mapping(address => uint256) dividendBalance;
 
-    /*
-    * @dev 1/rate
-    */
+    // @dev 1/rate
     uint256 private _rate;
 
-    /*
-    * @dev interval in seconds
-    */
+    // @dev interval in seconds
     uint256 private _interval;
 
-    constructor(uint256 interval) public {
+    constructor(uint256 rate, uint256 interval) public {
+        _rate = rate;
         _interval = interval;
     }
 
-    function updateRate(bytes32 rate, uint256 value) external onlyOwner() {
-        rates[rate] = value;
+    function calculate(address owner, address securityCoinContract, uint256 amount) public onlyOwner() {
+        uint256 secBal = securityBalance[owner][securityCoinContract];
+        uint256 lasUp = lastUpdated[owner][securityCoinContract];
 
-        emit rateUpdate(rate, rates[rate]);
-    }
+        uint256 dividend = secBal.div(_rate).mul((now.sub(lasUp)).div(_interval));
 
-    function updateSecurityBalance(address owner, uint amount) private {
-        securityBalance[owner] = securityBalance[owner].add(amount);
+        securityBalance[owner][securityCoinContract] = amount;
 
-        emit securityBalanceUpdate(owner, amount);
-    }
+        lastUpdated[owner][securityCoinContract] = now;
 
-    function calculate(address owner, address securityCoin, uint256 token) public onlyOwner() {
-        uint256 total = securityBalance[owner][securityCoin].div(_interval).mul(now.sub(lastUpdated[owner][securityCoin]));
+        dividendBalance[owner] = dividendBalance[owner].add(dividend);
 
-        securityBalance[owner][securityCoin] = token;
-
-        lastUpdated[owner][securityCoin] = now;
-
-        dividendBalance[owner] = dividendBalance[owner].add(total);
-
-        emit dividendsCalculated(owner, total);
-    
-
+        emit dividendsCalculated(owner, dividend, dividendBalance[owner]);
     }
 
     function withdraw(address owner) public onlyOwner() {
+        require(dividendBalance[owner] > 0);
+
         uint256 dividend = dividendBalance[owner];
 
         dividendBalance[owner] = 0;
